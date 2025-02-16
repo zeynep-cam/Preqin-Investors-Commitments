4e21b3a0f0c6e27fc652e7c651fad90993e4254e
zeynepmerveyanar <zmervey18@gmail.com>
2025-02-16 22:08:23 +0000

    Initialised .NET Solution

---
+++ Backend/Investors-Commitments/Investors-Commitments/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Investors_Commitments.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
